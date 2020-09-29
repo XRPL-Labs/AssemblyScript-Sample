43ad536e100a1df3380d7574309368e2ec1085e9
Wietse Wind <mail@wietse.com>
2020-09-29 21:58:04 +0200

    Thanks @MaxGraey - I needed coffee

--- assembly/index.ts
+++ assembly/index.ts
@@ -5,7 +5,7 @@ declare function jsHello(message?: i32): void
 
 export function main(): void {
   const someArray = [someFn(), anotherFn()]
-  jsHello(someArray[i32(Math.round(f64(Math.random())))])
+  jsHello(someArray[i32(Math.random() > 0.5)])
 }
 
 main()
